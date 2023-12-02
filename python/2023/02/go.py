ce12492b54a318db57aef433ca702d2aa81c6f90
Nick Barnes <nick@tarides.com>
2023-12-02 07:34:25 +0000

    Day 2.

---
+++ python/2023/02/go.py
@@ -0,0 +1,40 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'util'))
+
+import walk
+import file
+import re
+
+col_re = re.compile(' *([0-9]+) (red|blue|green)')
+
+class Game:
+    def __init__(self, s):
+        assert(s.startswith('Game '))
+        id, desc = s.split(':')
+        self.id = int(id[5:])
+        self.rounds = []
+        for r in desc.split(';'):
+            d = {}
+            for c in r.split(','):
+                m = col_re.match(c)
+                d[m.group(2)] = int(m.group(1))
+            self.rounds.append(d)
+    
+    def possible(self, cols):
+        return all(r.get(k,0) <= n for r in self.rounds for k,n in cols.items())
+
+    def power(self):
+        counts = [max(r.get(col,0) for r in self.rounds) for col in ['red','green','blue']]
+        return counts[0]*counts[1]*counts[2]
+
+def go(filename):
+    print(f"results from {filename}:")
+    lines = file.lines(filename)
+    games = [Game(l) for l in lines]
+    print(sum([g.id for g in games if g.possible({"red":12, "green":13, "blue":14})]))
+    print(sum(g.power() for g in games))
+
+if __name__ == '__main__':
+    for f in file.files(__file__):
+        go(f)
