3eca1d62ca2687c1e1c3c0f3a9731332603725c0
Nick Barnes <nick@tarides.com>
2023-12-06 11:44:34 +0000

    Day 6.

---
+++ python/2023/06/go.py
@@ -0,0 +1,32 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)),
+                             'util'))
+
+import walk
+import file
+import math
+import misc
+
+def winners(t,d):
+    first_winner = math.floor(t/2 - math.sqrt(t*t/4 -d) + 1)
+    last_winner = math.ceil(t/2 + math.sqrt(t*t/4 -d) - 1)
+    return last_winner - first_winner + 1
+
+def winner_prod(td):
+    return misc.prod(winners(int(t),int(d)) for t,d in td)
+
+def go(filename):
+    print(f"results from {filename}:")
+    words = file.words(filename)
+    assert(len(words) == 2)
+    assert(words[0][0] == 'Time:')
+    assert(words[1][0] == 'Distance:')
+    td = list(zip(words[0][1:], words[1][1:]))
+    print(f"part 1: {winner_prod(td)}")
+    td = [(''.join(words[0][1:]), ''.join(words[1][1:]))]
+    print(f"part 2: {winner_prod(td)}")
+
+if __name__ == '__main__':
+    for f in file.files(__file__):
+        go(f)
