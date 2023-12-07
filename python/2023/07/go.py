c496180a0d52ea5b8461f2c351db072b7182c982
Nick Barnes <nick@tarides.com>
2023-12-07 14:15:12 +0000

    A bit cleaner; less comprehension acrobatics.

--- python/2023/07/go.py
+++ python/2023/07/go.py
@@ -9,12 +9,9 @@ from collections import Counter
 
 @functools.total_ordering
 class Card:
-    def __init__(self,c, jokers = False):
+    def __init__(self, c, i):
         self.c = c
-        if jokers:
-            self.rank = 'J23456789TQKA'.index(c)
-        else:
-            self.rank = '23456789TJQKA'.index(c)
+        self.rank = i
 
     def __eq__(self, other):
         return self.c == other.c
@@ -25,8 +22,8 @@ class Card:
     def __repr__(self):
         return f"Card({self.c})"
 
-cards = {c:Card(c) for c in 'AKQJT98765432'}
-jw_cards = {c:Card(c, jokers=True) for c in 'AKQJT98765432'}
+cards = {c:Card(c,i) for i,c in enumerate('23456789TJQKA')}
+jw_cards = {c:Card(c, i) for i,c in enumerate('J23456789TQKA')}
 
 @functools.total_ordering
 class Hand:
@@ -39,41 +36,36 @@ class Hand:
             del c['J']
         else:
             self.jokers = 0
-        # fundamental hand type is the size of the maximum set
-        self.hand_type = max(c.values(), default=0) + self.jokers
+        # fundamental hand type ('kind') is the size of the maximum set
+        self.kind = max(c.values(), default=0) + self.jokers
         # two exceptions, which we can represent with intermediate values
-        if self.hand_type == 3 and len(c) == 2:
-            self.hand_type = 3.5 # full house
-        elif self.hand_type == 2 and len(c) == 3:
-            self.hand_type = 2.5 # two pair
+        if self.kind == 3 and len(c) == 2:
+            self.kind = 3.5 # full house
+        elif self.kind == 2 and len(c) == 3:
+            self.kind = 2.5 # two pair
 
     def __eq__(self, other):
         return self.hand == other.hand
 
     def __gt__(self, other):
-        return (self.hand_type > other.hand_type
-                or (self.hand_type == other.hand_type and
-                    self.cards > other.cards)) # uses Card ordering
+        return ((self.kind, self.cards) >
+                (other.kind, other.cards)) # uses Card ordering
     
     def __repr__(self):
-        return f"<{self.hand}, {self.hand_type}({self.jokers})>"
+        return f"<{self.hand}, {self.kind}({self.jokers})>"
 
-def find_ranks(hands, jokers = False):
+def winnings(lines, jokers = False):
     # pair all hands with their initial indices, and sort by hand
-    ranked = sorted((Hand(h, jokers), i) for i,h in enumerate(hands))
-    # enumerate the ranking, sort by initial index and yield the ranks
-    for i,k in sorted((i,k) for k, (_, i) in enumerate(ranked)):
-        yield k+1
+    ranked = sorted((Hand(h, jokers), i, int(b))
+                    for i, (h, b) in enumerate(lines))
+    # enumerate the ranking, multiply rank by the bid
+    return sum((k+1) * b for k, (_, i ,b) in enumerate(ranked))
 
 def go(filename):
     print(f"results from {filename}:")
     lines = file.words(filename)
-    ranks = find_ranks(h for (h,_) in lines)
-    print("part 1: ", sum(int(bid) * rank
-                          for (_, bid),rank in zip(lines,ranks)))
-    jw_ranks = find_ranks((h for (h,_) in lines), jokers = True)
-    print("part 2: ", sum(int(bid) * rank
-                          for (_, bid),rank in zip(lines,jw_ranks)))
+    print("part 1: ", winnings(lines))
+    print("part 2: ", winnings(lines, jokers = True))
 
 if __name__ == '__main__':
     for f in file.files(__file__):
