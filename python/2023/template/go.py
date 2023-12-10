8e583f61acf8af42ce717e8e7abbac594769e28c
Nick Barnes <nick@tarides.com>
2023-12-10 20:49:39 +0000

    Tweak template.

--- python/2023/template/go.py
+++ python/2023/template/go.py
@@ -2,9 +2,10 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)),
                              'util'))
-
 import walk
 import file
+import interval
+import misc
 
 def go(filename):
     print(f"results from {filename}:")
