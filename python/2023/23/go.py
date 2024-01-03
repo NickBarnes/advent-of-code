ddc5b4c86b9c188c4b5127b11c59a209e15def92
Nick Barnes <nick@tarides.com>
2024-01-03 10:05:05 +0000

    Use bitmaps for sets of nodes, making day 23 go 2x faster.

--- python/2023/23/go.py
+++ python/2023/23/go.py
@@ -21,9 +21,9 @@ def paths(grid, end, node, icy = True):
     paths = []
     seen = set([node])
     for n in neighbours(grid, seen, node, icy): # let's look this way
-        seen.add(n)
         d = 1
         while n != end:
+            seen.add(n)
             nx = list(neighbours(grid, seen, n, icy))
             if not nx: # nowhere to go
                 n = None
@@ -32,7 +32,6 @@ def paths(grid, end, node, icy = True):
                 break
             n = nx[0]
             d += 1
-            seen.add(n)
         if n: # we had somewhere to go
             paths.append((n, d))
     return paths
@@ -53,20 +52,23 @@ def graph(grid, start, end, icy):
                 seen.add(n)
     return graph
 
-def longest_from(graph, node, end, depth=0, seen = set()):
+def longest_from(graph, node, end, seen):
     if node == end:
         return 0
-    seen.add(node)
+    seen += node
     ls = [d + best
           for nx,d in graph[node]
-          if nx != node and nx not in seen
-          and (best := longest_from(graph, nx, end, depth+1, seen)) >= 0]
-    seen.remove(node)
+          if not (nx & seen)
+          and (best := longest_from(graph, nx, end, seen)) >= 0]
+    seen -= node
     return max(ls, default=-1)
 
 def longest(grid, start, end, icy):
     g = graph(grid, start, end, icy)
-    return longest_from(g, start, end)
+    # represent each node with a bit, and node sets with bit masks. 2x faster.
+    bit = {n: 1<<i for i,n in enumerate(g)}
+    g2 = {bit[n1]: [(bit[n2],w) for n2,w in l] for n1,l in g.items()}
+    return longest_from(g2, bit[start], bit[end], 0)
 
 def go(input):
     lines = parse.lines(input)
