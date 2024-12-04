d463d2dabc315a08c6211f0d8f146d01fb4ff251
Nick Barnes <nick@tarides.com>
2024-12-04 09:40:46 +0000

    Do better with less code :-).

--- python/2024/04/go.py
+++ python/2024/04/go.py
@@ -18,10 +18,11 @@ def check(grid, x, y, dx, dy, s, n):
     return False
 
 def cross(grid, x, y, dx, dy, s):
+    """Returns True if s appears in the grid starting at `(x,y)`, in
+    the direction `(dx,dy)`, and also, crossing itself at the first
+    letter, rotated 90 degrees clockwise."""
+
     if check(grid, x, y, dx, dy, s, 0):
-        cross_dx, cross_dy = -dy, dx # counter-clockwise
-        if check(grid, x+dx-cross_dx, y+dy-cross_dy, cross_dx, cross_dy, s, 0):
-            return True
         cross_dx, cross_dy = dy, -dx # clockwise
         if check(grid, x+dx-cross_dx, y+dy-cross_dy, cross_dx, cross_dy, s, 0):
             return True
@@ -35,17 +36,13 @@ def count(grid, s):
                if check(grid, x, y, dx, dy, s, 0))
 
 def crosses(grid, s):
-    # every cross will be counted twice. Could do better.
-    count = sum(1
-                for y in range(len(grid))
-                for x in range(len(grid[0]))
-                for dx,dy in diags()
-                if cross(grid, x, y, dx, dy, s))
-    assert count % 2 == 0
-    return count // 2
+    return sum(1
+               for y in range(len(grid))
+               for x in range(len(grid[0]))
+               for dx,dy in diags()
+               if cross(grid, x, y, dx, dy, s))
     
 def go(input):
     grid = parse.lines(input)
     print(count(grid,'XMAS'))
     print(crosses(grid,'MAS'))
-    
